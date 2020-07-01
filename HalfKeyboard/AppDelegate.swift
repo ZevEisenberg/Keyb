53641b8426fb7aa94eaef3ebaecab64be637eddd
Zev Eisenberg <zev@zeveisenberg.com>
2020-07-01 15:48:00 -0400

    Initial commit.

---
+++ HalfKeyboard/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HalfKeyboard
+//
+//  Created by Zev Eisenberg on 7/1/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HalfKeyboard/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HalfKeyboard
+//
+//  Created by Zev Eisenberg on 7/1/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
