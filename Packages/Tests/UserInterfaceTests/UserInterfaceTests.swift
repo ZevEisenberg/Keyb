aa37a883718ef0282fa9a043173defee3ec7e942
Zev Eisenberg <zev@zeveisenberg.com>
2021-09-19 11:29:05 -0400

    Add test for already-has-permission case.

--- Packages/Tests/UserInterfaceTests/UserInterfaceTests.swift
+++ Packages/Tests/UserInterfaceTests/UserInterfaceTests.swift
@@ -64,4 +64,47 @@ final class UserInterfaceTests: XCTestCase {
         XCTAssertFalse(eventHandlerIsRunning)
     }
 
+    func testAlreadyHasPermission() {
+        let mainQueue = DispatchQueue.test
+
+        var eventHandlerIsRunning = false
+
+        let store = TestStore(
+            initialState: .init(mode: .hasAccessibilityPermission(isRunning: false)),
+            reducer: userInterfaceReducer,
+            environment: .init(
+                accessibilityClient: .init(isCurrentlyTrusted: { true }),
+                eventHandlerClient: .init(
+                    isEnabled: { eventHandlerIsRunning },
+                    startProvisional: { true },
+                    startActive: {
+                        eventHandlerIsRunning = true
+                        return true
+                    },
+                    stop: {
+                        eventHandlerIsRunning = false
+                    }
+                ),
+                mainQueue: mainQueue.eraseToAnyScheduler()
+            )
+        )
+
+        store.send(.checkForPermissions)
+        store.receive(.permissionChanged(hasAccessibilityPermission: true))
+
+        mainQueue.advance(by: 5)
+
+        store.send(.startObservingEvents) {
+            $0.mode = .hasAccessibilityPermission(isRunning: true)
+        }
+
+        XCTAssertTrue(eventHandlerIsRunning)
+
+        store.send(.stopObservingEvents) {
+            $0.mode = .hasAccessibilityPermission(isRunning: false)
+        }
+
+        XCTAssertFalse(eventHandlerIsRunning)
+    }
+
 }
