855c1327e8d0679d12199d3c79789ff1f8882904
Zev Eisenberg <zev@zeveisenberg.com>
2022-11-28 16:52:24 -0500

    Modernize Package.swift.

--- Packages/Package.swift
+++ Packages/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -29,7 +29,7 @@ let package = Package(
         .singleTargetLibrary("HumanReadable"),
     ],
     dependencies: [
-        .package(name: "swift-composable-architecture", url: "https://github.com/pointfreeco/swift-composable-architecture", .upToNextMinor(from: "0.28.0")),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", exact: "0.28.1"),
     ],
     targets: [
         .target(
