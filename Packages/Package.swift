f9ff499842354d6d3247211c442616a7c4a51841
Zev Eisenberg <zev@zeveisenberg.com>
2023-11-09 17:04:06 -0500

    Update to TCA 1.3.0.

--- Packages/Package.swift
+++ Packages/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         .singleTargetLibrary("HumanReadable"),
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", exact: "1.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", exact: "1.3.0"),
     ],
     targets: [
         .target(
